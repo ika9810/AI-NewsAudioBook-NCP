6296ba7fae21492db75604cf547c07a8e7859bf7
JONGHEON LEE <ika7204@naver.com>
2021-09-13 11:39:40 +0900

    Delete GoogleTrend directory

--- GoogleTrend/test.js
+++
@@ -1,8 +0,0 @@
-const trendsService = require('./google_trends_service');
-
-trendsService.getDailySearchTrends()
-    .then(trendsList => {
-        console.log(JSON.stringify(trendsList, null, 2));
-    })
-    .catch(err => console.err);
-;
